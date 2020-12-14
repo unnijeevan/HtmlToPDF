85f6aedda6842c2f5a72ed5a0b21c7cbb962e4cc
jeevan <jeevan@beforepay.co>
2020-12-15 00:17:53 +1100

    Add project files.

---
+++ HtmlToPDF/Function.cs
@@ -0,0 +1,79 @@
+using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Threading.Tasks;
+
+using Amazon.Lambda.Core;
+using Amazon.Lambda.S3Events;
+using Amazon.S3;
+using Amazon.S3.Util;
+using HeadlessChromium.Puppeteer.Lambda.Dotnet;
+using Microsoft.Extensions.Logging;
+
+// Assembly attribute to enable the Lambda function's JSON input to be converted into a .NET class.
+[assembly: LambdaSerializer(typeof(Amazon.Lambda.Serialization.SystemTextJson.DefaultLambdaJsonSerializer))]
+
+namespace HtmlToPDF
+{
+    public class Function
+    {
+        IAmazonS3 S3Client { get; set; }
+
+        /// <summary>
+        /// Default constructor. This constructor is used by Lambda to construct the instance. When invoked in a Lambda environment
+        /// the AWS credentials will come from the IAM role associated with the function and the AWS region will be set to the
+        /// region the Lambda function is executed in.
+        /// </summary>
+        public Function()
+        {
+            S3Client = new AmazonS3Client();
+        }
+
+        /// <summary>
+        /// Constructs an instance with a preconfigured S3 client. This can be used for testing the outside of the Lambda environment.
+        /// </summary>
+        /// <param name="s3Client"></param>
+        public Function(IAmazonS3 s3Client)
+        {
+            this.S3Client = s3Client;
+        }
+
+        /// <summary>
+        /// This method is called for every Lambda invocation. This method takes in an S3 event object and can be used 
+        /// to respond to S3 notifications.
+        /// </summary>
+        /// <param name="evnt"></param>
+        /// <param name="context"></param>
+        /// <returns></returns>
+        public async Task<string> FunctionHandler(S3Event evnt, ILambdaContext context)
+        {
+            var s3Event = evnt.Records?[0].S3;
+            if (s3Event == null)
+            {
+                return null;
+            }
+
+            try
+            {
+                using var response = await S3Client.GetObjectAsync(s3Event.Bucket.Name, s3Event.Object.Key);
+                var browserLauncher = new HeadlessChromiumPuppeteerLauncher(new LoggerFactory());
+
+                using var browser = await browserLauncher.LaunchAsync();
+                using var page = await browser.NewPageAsync();
+                using var stream = response.ResponseStream;
+                using var reader = new StreamReader(stream);
+                await page.SetContentAsync(reader.ReadToEnd());
+                var pdf = await page.PdfStreamAsync();
+                await S3Client.UploadObjectFromStreamAsync("invoice2pdf", $"{s3Event.Object.Key}.pdf", pdf, null);
+                return response.Headers.ContentType;
+            }
+            catch (Exception e)
+            {
+                context.Logger.LogLine(e.Message);
+                context.Logger.LogLine(e.StackTrace);
+                throw;
+            }
+        }
+    }
+}
